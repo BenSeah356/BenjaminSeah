1fc89d1e385e1e616c521d5fc56730a14d849f58
BenSeah356 <34400148+BenSeah356@users.noreply.github.com>
2020-06-15 10:50:44 -0500

    Add files via upload

---
+++ ConsoleApplication1/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public int WalletId { get; set; }
+        public string CardType { get; set; }
+        public float Balance { get; set; }
+        public float Interest { get; set; }
+
+        public float calculateCardInterest(float Balance, string CardType)
+        {
+            Interest = 0;
+            if (CardType == "Visa")
+            {
+                Interest = 0.1f * Balance;
+            }
+            else if (CardType == "MC")
+            {
+                Interest = .05f * Balance;
+            }
+            else if (CardType == "Discover")
+            {
+                Interest = .01f * Balance;
+            }
+            else
+            {
+                Interest = 0f;
+            }
+            return Interest;
+        }
+    }
+}
